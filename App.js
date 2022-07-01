fc23509fdc13ef4395720f96efe79d2b1242df4f
Vince Ding <vince.ding@gmail.com>
2022-07-01 22:27:12 +0800

    version 1

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View,  } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./BlockRGB";
+
+function HomeScreen() {
+ const [colorArray, setColorArray] = useState([
+   { red: 255, green: 0, blue: 0, id: "0" },
+   { red: 0, green: 255, blue: 0, id: "1" },
+   { red: 0, green: 0, blue: 255, id: "2" },
+ ]);
+
+ function renderItem({ item }) {
+   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+ }
+
+ function addColor() {
+  setColorArray([
+    ...colorArray,
+    {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: `${colorArray.length}`,
+    },
+  ]);
+}
+
+return (
+  <View style={styles.container}>
+    <TouchableOpacity
+      style={{ height: 40, justifyContent: "center" }}
+      onPress={addColor}
+    >
+      <Text style={{ color: "black", fontSize: 11, backgroundColor: 'white', padding: 5, width: 120, textAlign: 'center', borderWidth: 1, borderRadius: 10, borderColor: 'grey' }}>ADD COLOUR</Text>
+    </TouchableOpacity>
+    <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+  </View>
+);
+
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Colour List" component={HomeScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+ },
+ list: {
+   width: "100%",
+ },
 });
+
+
